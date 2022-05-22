59126c3fcba537657435fbf220a21196aa651872
reddyarun59 <reddyarun59@yahoo.com>
2022-05-22 16:00:36 +0530

    REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express= require("express")
+const colors= require("colors")
 const dotenv=require("dotenv").config()
 const {errorHandler} = require("./middleware/errorMiddleware")
+const {connectDB}=require("./config/db")
 //const router=require("./routes/goalRoutes")
 const port=process.env.PORT||5000
+connectDB();
 
 const app=express()
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended:false}))
